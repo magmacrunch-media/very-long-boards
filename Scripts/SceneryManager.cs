1ffea97cf734f354b380ec1768e3816805842daf
magmacrunchmedia <magmacrunchmedia@gmail.com>
2026-06-30 21:50:48 -0400

    brighter house windows
    - Window color brighter blue (0.78, 0.9, 1.0)
    - Higher opacity (0.9)

--- Scripts/SceneryManager.cs
+++ Scripts/SceneryManager.cs
@@ -393,7 +393,7 @@ public class SceneryManager
         doorMat.Roughness = 0.65f;
 
         var winMat = new StandardMaterial3D();
-        winMat.AlbedoColor = new Color(0.75f, 0.88f, 0.98f, 0.88f);
+        winMat.AlbedoColor = new Color(0.78f, 0.9f, 1f, 0.9f);
         winMat.Transparency = BaseMaterial3D.TransparencyEnum.Alpha;
         winMat.EmissionEnabled = true;
         winMat.Emission = new Color(0.1f, 0.08f, 0.04f);
